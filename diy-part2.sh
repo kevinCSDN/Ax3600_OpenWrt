260dd7c50e620ab002c0182d95c90514bd9da806
kevinCSDN <81788909+kevinCSDN@users.noreply.github.com>
2023-11-24 15:29:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,21 +11,21 @@
 #
 
 # Modify default IP
-rm -rf feeds/luci/applications/luci-app-argon-config
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-vssr
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/packages/net/v2ray-core
-rm -rf feeds/packages/net/v2ray-plugin
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/xray-plugin
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+#rm -rf feeds/luci/applications/luci-app-argon-config
+#rm -rf feeds/luci/applications/luci-app-openclash
+#rm -rf feeds/luci/applications/luci-app-vssr
+#rm -rf feeds/luci/applications/luci-app-ssr-plus
+#rm -rf feeds/luci/applications/luci-app-passwall
+#rm -rf feeds/luci/themes/luci-theme-argon
+#rm -rf feeds/packages/net/chinadns-ng
+#rm -rf feeds/packages/net/v2ray-core
+#rm -rf feeds/packages/net/v2ray-plugin
+#rm -rf feeds/packages/net/xray-core
+#rm -rf feeds/packages/net/xray-plugin
+#sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+#git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
-svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
-git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+#svn export https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
+#svn export https://github.com/xiaorouji/openwrt-passwall2/trunk/luci-app-passwall2 package/luci-app-passwall2
+#git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
